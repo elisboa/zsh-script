437e8ce179b291135bb606874e83d145adbaadc9
Erick Garcia <erickgarciagodoy@gmail.com>
2020-05-14 18:01:25 -0300

    Versão 1.6.9

--- zsh-install.sh
+++ zsh-install.sh
@@ -51,10 +51,9 @@ SPACESHIP_CHAR_SYMBOL="->"
 SPACESHIP_CHAR_SUFFIX=" "' >> ~/.zshrc
 
 #Instalação do pluguins
-sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)" -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
 
 sudo echo "
-### End of ZInit's installer chunk
 zinit light zdharma/fast-syntax-highlighting
 zinit light zsh-users/zsh-autosuggestions
 zinit light zsh-users/zsh-completions" >> ~/.zshrc 
