2b2c9311a357d57c4b5c0d97f778b005782a0e47
Erick Garcia <erickgarciagodoy@gmail.com>
2020-05-14 15:58:53 -0300

    Versão 1.5

--- zsh-install.sh
+++ zsh-install.sh
@@ -3,18 +3,13 @@
 # Descrição: Instalação do Oh-my-zsh + Pluguins de autocomplete e tema Spaceship padrão
 #!/bin/bash
 cd ~ 
-
 sudo apt-get update
-
 sudo apt-get upgrade -y
-
 sudo add-apt-repository ppa:git-core/ppa 
-
 sudo apt update && sudo apt install git zsh -y
 
 #Define o zsh como shell padrão
-sudo echo "
-# if running in terminal...
+sudo echo "# if running in terminal...
 if test -t 1; then
 # ...start zsh
 exec zsh
@@ -35,21 +30,20 @@ sudo ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTO
 
 sed -i 's/robbyrussell/spaceship/' ~/.zshrc
 
-echo '
-#Configuração da interface
+echo '#Configuração da interface do terminal
 SPACESHIP_PROMPT_ORDER=(
-  user          # Username section
-  dir           # Current directory section
-  host          # Hostname section
-  git           # Git section (git_branch + git_status)
-  hg            # Mercurial section (hg_branch  + hg_status)
-  exec_time     # Execution time
-  line_sep      # Line break
-  vi_mode       # Vi-mode indicator
-  jobs          # Background jobs indicator
-  exit_code     # Exit code section
-  char          # Prompt character
-  docker        # Docker Version
+   user          # Username section
+   dir           # Current directory section
+   host          # Hostname section
+   git           # Git section (git_branch + git_status)
+   hg            # Mercurial section (hg_branch  + hg_status)
+   exec_time     # Execution time
+   line_sep      # Line break
+   vi_mode       # Vi-mode indicator
+   jobs          # Background jobs indicator
+   exit_code     # Exit code section
+   char          # Prompt character
+   docker        # Docker Version
 )
 SPACESHIP_USER_SHOW=always
 SPACESHIP_PROMPT_ADD_NEWLINE=false
@@ -59,8 +53,8 @@ SPACESHIP_CHAR_SUFFIX=" "' >> ~/.zshrc
 #Instalação do pluguins
 sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)" -y
 
-echo "
-zinit light zdharma/fast-syntax-highlighting
+echo "zinit light zdharma/fast-syntax-highlighting
 zinit light zsh-users/zsh-autosuggestions
 zinit light zsh-users/zsh-completions" >> ~/.zshrc 
+
 echo -e "Instalação concluída com sucesso!!\nFeito por: Erick Garcia"
