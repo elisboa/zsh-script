03ff0e82c73e19e16523ca2e1bd4bf467698995e
Erick Garcia <erickgarciagodoy@gmail.com>
2020-05-15 16:51:37 -0300

    test

--- tools-zsh-install.sh
+++ tools-zsh-install.sh
@@ -184,10 +184,10 @@ EOF
 #Instalação do Spaceship Theme
 cd ~
 
-sudo git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+# sudo git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
 
-sudo ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+# sudo ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
-sed -i 's/robbyrussell/spaceship/' ~/.zshrc
+# sed -i 's/robbyrussell/spaceship/' ~/.zshrc
 
 echo "Instalação concluída com sucesso!!\nFeito por: Erick Garcia"
