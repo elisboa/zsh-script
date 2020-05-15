f95e391f2dcc7fa30e0aa3210f9e4dc56c397c24
Erick Garcia <erickgarciagodoy@gmail.com>
2020-05-15 14:49:39 -0300

    Script divido em duas partes

--- zsh-install.sh
+++ zsh-install.sh
@@ -1,7 +1,8 @@
-#Script de instalação - ZSH + Pluguins + Autocomplete 
-# Autor: Erick Garcia Kun
-# Descrição: Instalação do Oh-my-zsh + Pluguins de autocomplete e tema Spaceship padrão
 #!/bin/bash
+# Script de instalação - ZSH shell
+# Autor: Erick Garcia Kun
+# Descrição: Instalação do Oh-my-zsh shell padrão
+
 cd ~ 
 sudo apt-get update
 sudo apt-get upgrade -y
@@ -16,46 +17,5 @@ exec zsh
 fi" > previa.txt
 cat ~/.bashrc >> previa.txt
 sudo mv previa.txt ~/.bashrc
-
-#Instalação do ZSH
-cd ~
-
-sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" -y
-
-cd ~
-
-sudo git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-
-sudo ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-
-sed -i 's/robbyrussell/spaceship/' ~/.zshrc
-
-sudo echo '#Configuração da interface do terminal
-SPACESHIP_PROMPT_ORDER=(
-   user          # Username section
-   dir           # Current directory section
-   host          # Hostname section
-   git           # Git section (git_branch + git_status)
-   hg            # Mercurial section (hg_branch  + hg_status)
-   exec_time     # Execution time
-   line_sep      # Line break
-   vi_mode       # Vi-mode indicator
-   jobs          # Background jobs indicator
-   exit_code     # Exit code section
-   char          # Prompt character
-   docker        # Docker Version
-)
-SPACESHIP_USER_SHOW=always
-SPACESHIP_PROMPT_ADD_NEWLINE=false
-SPACESHIP_CHAR_SYMBOL="->"
-SPACESHIP_CHAR_SUFFIX=" "' >> ~/.zshrc
-
-#Instalação do pluguins
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
-
-sudo echo "
-zinit light zdharma/fast-syntax-highlighting
-zinit light zsh-users/zsh-autosuggestions
-zinit light zsh-users/zsh-completions" >> ~/.zshrc 
 
 echo -e "Instalação concluída com sucesso!!\nFeito por: Erick Garcia"
