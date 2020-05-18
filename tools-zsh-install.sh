e5a25713d7f86cc02162360160f19411cfb3680e
Erick Garcia <erickgarciagodoy@gmail.com>
2020-05-18 09:59:33 -0300

    Versão 2.5

--- tools-zsh-install.sh
+++ tools-zsh-install.sh
@@ -5,19 +5,24 @@
 
 cd ~
 
+#Instala ZSH Tools via curl
 sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" -y
 
 #Instalação do Spaceship Theme
 cd ~
 
-sudo git clone https://github.com/denysdovhan/spaceship-prompt.git "./.oh-my-zsh/custom/themes/spaceship-prompt"
+#Clona o tema Spaceship
+sudo git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
 
-sudo ln -s "./.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
+#Essa parte por algum motivo não consegue realizar a criação de link simbólico
+sudo ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
+#Editar o ~/.zshrc para setar o tema para Spaceship
 sed -i 's/robbyrussell/spaceship/' ~/.zshrc
 
+
+#Adição de configuração do spaceship no arquivo ~/.zshrc
 sudo echo "#Configuração da interface spaceship do terminal
 SPACESHIP_PROMPT_ORDER=(
    user          # Username section
@@ -39,12 +44,14 @@ SPACESHIP_CHAR_SYMBOL="->"
 SPACESHIP_CHAR_SUFFIX=" "" >> ~/.zshrc
 
 
-
 #Instalação do Zinit com plubuins
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
 
+#Adição dos pluguins 
 sudo echo "zinit light zdharma/fast-syntax-highlighting
 zinit light zsh-users/zsh-autosuggestions
 zinit light zsh-users/zsh-completions" >> ~/.zshrc
+
+#Ao iniciar o terminal novamente, ele pode indicar erros como, sem permissão para criar diretórios, do script na do zinit, e até mesmo permissão negada ao criar links simbólicos
 
 echo "Instalação concluída com sucesso!!\nFeito por: Erick Garcia"
